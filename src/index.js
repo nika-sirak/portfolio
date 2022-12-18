91ac666ca176d0e5e357c552f61354b88fea3730
nika-sirak <7nicka@gmail.com>
2022-12-18 01:01:18 +0000

    add animation

--- src/index.js
+++ src/index.js
@@ -25,3 +25,31 @@ function onMenuLinkEl() {
     document.body.classList.remove("lock");
   }
 }
+
+//scroll animation
+const heroObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && entry.target.classList.contains("hero-anim")) {
+      entry.target.classList.add("animate__slideInLeft");
+    } else {
+      entry.target.classList.remove("animate__slideInLeft");
+    }
+    if (entry.isIntersecting && !entry.target.classList.contains("hero-anim")) {
+      entry.target.classList.add("animate__slideInRight");
+    } else {
+      entry.target.classList.remove("animate__slideInRight");
+    }
+  });
+});
+const heroEl = document.querySelectorAll("[data-action='animate-hero']");
+heroEl.forEach((el) => heroObserver.observe(el));
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate__slideInUp");
+    }
+  });
+});
+const mainEl = document.querySelectorAll("[data-action='animate']");
+mainEl.forEach((el) => observer.observe(el));
