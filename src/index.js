d98d8f0a44bf150b4212039e3fbf1d1922650819
nika-sirak <7nicka@gmail.com>
2023-01-11 00:51:31 +0000

    bugfix mouse

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import "./js/animation";
 import "./js/burger-menu";
 import "./js/theme-switch";
 import "./js/scroll-to-top";
+
+const mouse = document.querySelector(".mouse");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 60) {
+    mouse.classList.add("visually-hidden");
+  } else {
+    mouse.classList.remove("visually-hidden");
+  }
+};
